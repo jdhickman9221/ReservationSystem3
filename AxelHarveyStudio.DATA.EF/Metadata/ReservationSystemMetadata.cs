3891302abae7eff2921312329f684fa531e79d56
Jdhickman9221 <jdhickman816@outlook.com>
2021-06-07 14:55:20 -0500

    ++Added Data Layer (forgot to create a new branch for this) ++Added MetaData folder in Data Layer.

---
+++ AxelHarveyStudio.DATA.EF/Metadata/ReservationSystemMetadata.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AxelHarveyStudio.DATA.EF/*.Metadata*/
+{
+    class ReservationSystemMetadata
+    {
+
+        #region Location
+
+        #endregion
+
+        #region OwnerAsset
+
+        #endregion
+
+        #region Reservation
+
+        #endregion
+
+        #region UserDetail
+
+        #endregion
+
+     
+
+
+
+
+
+
+    }
+}
