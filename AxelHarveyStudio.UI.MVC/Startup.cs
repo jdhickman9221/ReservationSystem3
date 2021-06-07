356f2c3dd808fe05d905439b4417110e6cefe946
Jdhickman9221 <jdhickman816@outlook.com>
2021-06-07 13:31:21 -0500

    -Added Identity Samples NuGet Package

--- AxelHarveyStudio.UI.MVC/Startup.cs
+++ AxelHarveyStudio.UI.MVC/Startup.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using Owin;
 
-[assembly: OwinStartupAttribute(typeof(AxelHarveyStudio.UI.MVC.Startup))]
 namespace AxelHarveyStudio.UI.MVC
 {
     public partial class Startup
